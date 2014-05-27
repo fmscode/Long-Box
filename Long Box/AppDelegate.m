dc81fbc0c4986c6db459c062f2336a014dd8bcf8
Frank Michael Sanchez <orion1701@me.com>
2014-05-27 16:02:36 -0400

    Initial Commit

---
+++ Long Box/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  Long Box
+//
+//  Created by Frank Michael on 5/27/14.
+//  Copyright (c) 2014 Frank Michael Sanchez. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ Long Box/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Long Box
+//
+//  Created by Frank Michael on 5/27/14.
+//  Copyright (c) 2014 Frank Michael Sanchez. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
